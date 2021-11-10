668b85fdc57142157a5742e85acc021f6a41d488
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-11-09 16:47:41 -0800

    Adding nonce to styles too

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
 // https://github.com/hotwired/turbo/issues/294#issuecomment-877842232
 document.addEventListener("turbo:before-fetch-request", (event) => {
   // Turbo Drive does not send a referrer like turbolinks used to, so let's simulate it here
@@ -6,13 +10,17 @@ document.addEventListener("turbo:before-fetch-request", (event) => {
 });
 
 // https://github.com/hotwired/turbo/issues/294#issuecomment-877842232
-document.addEventListener("turbo:before-cache", function() {
-  let scriptTagsToAddNonces = document.querySelectorAll("script[nonce]");
-  for (var element of scriptTagsToAddNonces) {
-    element.setAttribute("nonce", element.nonce);
+// Also borrowing from https://github.com/turbolinks/turbolinks/issues/430
+document.addEventListener("turbo:before-cache", () => {
+  const styleTags = document.querySelectorAll("style");
+  const scriptTags = document.querySelectorAll("script");
+  const pageNonce = document.querySelector("meta[name='csp-nonce']");
+  if (pageNonce) {
+    if (styleTags) {
+      styleTags.forEach(tag => { tag.nonce = pageNonce.content; });
+    }
+    if (scriptTags) {
+      scriptTags.forEach(tag => { tag.nonce = pageNonce.content; });
+    }
   }
 });
-
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
