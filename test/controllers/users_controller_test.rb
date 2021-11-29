3909ec90884b6d3afa45c9ab7ff47e1c881b4cf8
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-11-29 00:20:00 -0800

    Add initial tests for create a new user actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,6 +10,11 @@ class UsersControllerTest < ApplicationControllerTestCase
   end
 
   describe "#new" do
+    it "loads the signup form" do
+      get signup_path
+      assert_response :success
+      assert_select "h2.title", "Sign up"
+    end
   end
 
   describe "#edit" do
@@ -21,12 +26,30 @@ class UsersControllerTest < ApplicationControllerTestCase
   end
 
   describe "#create" do
+    it "creates a user" do
+      assert_difference "User.count", 1 do
+        post users_path, params: { user: { email: "jack@dafox.com", username: "jackthecat", name: "Jack", password: "super_secret", password_confirmation: "super_secret" } }
+      end
+      assert_equal "jackthecat", User.last.username
+      assert_redirected_to login_url
+    end
+
+    it "sends a welcome email to the new user" do
+      assert_emails 1 do
+        post users_path, params: { user: { email: "jack@dafox.com", username: "jackthecat", name: "Jack", password: "super_secret", password_confirmation: "super_secret" } }
+      end
+      last_email  = ActionMailer::Base.deliveries.last
+      assert_equal "Welcome to jpstudy", last_email.subject
+      assert_equal ["jack@dafox.com"], last_email.to
+    end
   end
 
   describe "#update" do
     it "returns not found when accessed by a different user" do
       login(users(:carl))
-      patch user_path(users(:daisy)), params: { user: { email: "i_changed_your_email" } }
+      assert_no_changes "User.find(users(:daisy).id).email" do
+        patch user_path(users(:daisy)), params: { user: { email: "i_changed_your_email" } }
+      end
       assert_response :not_found
     end
   end
@@ -34,7 +57,9 @@ class UsersControllerTest < ApplicationControllerTestCase
   describe "#destroy" do
     it "returns not found when accessed by a different user" do
       login(users(:carl))
-      delete user_path(users(:daisy))
+      assert_no_difference "User.count" do
+        delete user_path(users(:daisy))
+      end
       assert_response :not_found
     end
   end
