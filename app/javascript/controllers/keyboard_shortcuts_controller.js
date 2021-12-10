e3b146885212dc1b26556dea9718d6cbc5f80639
Joshua Hunsche Jones <jhunschejones@github.com>
2021-12-10 15:07:53 -0800

    Refactor keyboard shortcuts controller class

--- app/javascript/controllers/keyboard_shortcuts_controller.js
+++ app/javascript/controllers/keyboard_shortcuts_controller.js
@@ -2,182 +2,97 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  connect() {
-    let keysPressed = {}
-    document.onkeydown = function(e) {
-      if (window.disableJpstudyKeyboardShorcuts) {
-        return;
-      }
-      switch (e.key) {
-        case "Shift":
-          keysPressed["shift"] = true;
-          break;
-        case "Alt":
-          keysPressed["option"] = true;
-          break;
-        case "Escape":
-          keysPressed["escape"] = true;
-          break;
-        case "Enter":
-          keysPressed["enter"] = true;
-          break;
-        case "Meta":
-          keysPressed["command"] = true;
-          break;
+  initialize() {
+    this.keysPressed = {}
+    document.onkeydown = this.execute.bind(this);
+    document.onkeyup = this.reset.bind(this);
+  }
 
-        // === normal key variants
-        case "a":
-          keysPressed["a"] = true;
-          break;
-        case "c":
-          keysPressed["c"] = true;
-          break;
-        case "d":
-          keysPressed["d"] = true;
-          break;
-        case "e":
-          keysPressed["e"] = true;
-          break;
-        case "f":
-          keysPressed["f"] = true;
-          break;
-        case "i":
-          keysPressed["i"] = true;
-          break;
-        case "n":
-          keysPressed["n"] = true;
-          break;
-        case "s":
-          keysPressed["s"] = true;
-          break;
-        case "t":
-          keysPressed["t"] = true;
-          break;
-        case "u":
-          keysPressed["u"] = true;
-          break;
-        case "w":
-          keysPressed["w"] = true;
-          break;
+  reset() {
+    this.keysPressed = {}
+  }
 
-        // === `shift + option + key` variants
-        // case "Å":
-        //   keysPressed["a"] = true;
-        //   break;
-        // case "Ç":
-        //   keysPressed["c"] = true;
-        //   break;
-        // case "Î":
-        //   keysPressed["d"] = true;
-        //   break;
-        // case "´":
-        //   keysPressed["e"] = true;
-        //   break;
-        // case "Ï":
-        //   keysPressed["f"] = true;
-        //   break;
-        // case "ˆ":
-        //   keysPressed["i"] = true;
-        //   break;
-        // case "˜":
-        //   keysPressed["n"] = true;
-        //   break;
-        // case "Í":
-        //   keysPressed["s"] = true;
-        //   break;
-        // case "ˇ":
-        //   keysPressed["t"] = true;
-        //   break;
-        // case "¨":
-        //   keysPressed["u"] = true;
-        //   break;
-        // case "„":
-        //   keysPressed["w"] = true;
-        //   break;
+  execute(e) {
+    switch (e.key) {
+      case "Alt":
+        this.keysPressed["option"] = true;
+        break;
+      case "Meta":
+        this.keysPressed["command"] = true;
+        break;
+      default:
+        this.keysPressed[e.key.toLowerCase()] = true;
+    }
+
+    if (this.keysPressed["escape"]) {
+      const flashCloseButtons = document.querySelectorAll("#flashes .flash .close-flash-button");
+      if (flashCloseButtons.length > 0) {
+        return flashCloseButtons.forEach(button => { button.click(); });
       }
 
-      if (keysPressed["escape"]) {
-        const flashCloseButtons = document.querySelectorAll("#flashes .flash .close-flash-button");
-        if (flashCloseButtons.length > 0) {
-          return flashCloseButtons.forEach(button => { button.click(); });
-        }
-
-        const newWordBackButton = document.querySelector(".words-list-page #new-word-form .back-button");
-        if (newWordBackButton) {
-          return newWordBackButton.click();
-        }
-
-        const editWordBackButton = document.querySelector(".words-list-page .words-list-container .word-form .back-button");
-        if (editWordBackButton) {
-          return editWordBackButton.click();
-        }
-
-        // escape will "unfocus" a currently focused input or textarea field
-        const focusedInputFields = document.querySelectorAll("input:focus,textarea:focus");
-        if (focusedInputFields.length > 0) {
-          return focusedInputFields.forEach(field => { field.blur(); });
-        }
-
-        // escape will go back one page from the search page
-        if (window.location.pathname == "/words/search") {
-          return history.back();
-        }
-      }
-      if (keysPressed["enter"]) {
-        const flashCloseButtons = document.querySelectorAll("#flashes .flash .close-flash-button");
-        if (flashCloseButtons.length > 0) {
-          return flashCloseButtons.forEach(button => { button.click(); });
-        }
+      const newWordBackButton = document.querySelector(".words-list-page #new-word-form .back-button");
+      if (newWordBackButton) {
+        return newWordBackButton.click();
       }
 
-      const focusedInputFieldsArePresent = document.querySelectorAll("input:focus,textarea:focus").length > 0;
-      if (focusedInputFieldsArePresent) {
-        // If we've got a form up with a field in focus, don't execute any more shortcuts
-        return;
+      const editWordBackButton = document.querySelector(".words-list-page .words-list-container .word-form .back-button");
+      if (editWordBackButton) {
+        return editWordBackButton.click();
       }
 
-      if (keysPressed["command"] || keysPressed["option"] || keysPressed["shift"]) {
-        // None of our remaining shortcuts use combo keys
-        return;
+      // escape will "unfocus" a currently focused input or textarea field
+      const focusedInputFields = document.querySelectorAll("input:focus,textarea:focus");
+      if (focusedInputFields.length > 0) {
+        return focusedInputFields.forEach(field => { field.blur(); });
       }
 
-      if (keysPressed["w"] || keysPressed["a"]) {
-        e.preventDefault(); // stops key value from being entered into an input field
-        return Turbo.visit("/words");
+      // escape will go back one page from the search page
+      if (window.location.pathname == "/words/search") {
+        return history.back();
       }
-      if (keysPressed["c"]) {
-        e.preventDefault();
-        return Turbo.visit("/words?filter=cards_not_created&order=oldest_first");
+    }
+    if (this.keysPressed["enter"]) {
+      const flashCloseButtons = document.querySelectorAll("#flashes .flash .close-flash-button");
+      if (flashCloseButtons.length > 0) {
+        return flashCloseButtons.forEach(button => { button.click(); });
       }
-      if (keysPressed["n"]) {
-        e.preventDefault();
-        const newWordBtton = document.querySelector("#new-word-form .new-word");
-        if (newWordBtton) {
-          return newWordBtton.click();
-        }
-        return Turbo.visit("/words/new");
-      }
-      if (keysPressed["f"]) {
-        e.preventDefault();
-        return Turbo.visit("/words/search");
-      }
-      if (keysPressed["e"]) {
-        e.preventDefault();
-        return Turbo.visit("/words/export");
-      }
-      if (keysPressed["i"]) {
-        e.preventDefault();
-        return Turbo.visit("/words/import");
-      }
-      if (keysPressed["s"]) {
-        e.preventDefault();
-        return document.querySelector("#stats-nav-link").click();
-      }
-    };
+    }
 
-    document.onkeyup = function() {
-      // reset on key up
-      keysPressed = {}
+    const focusedInputFieldsArePresent = document.querySelectorAll("input:focus,textarea:focus").length > 0;
+    if (focusedInputFieldsArePresent) {
+      // If we've got a form up with a field in focus, don't execute any more shortcuts
+      return;
+    }
+
+    if (this.keysPressed["command"] || this.keysPressed["option"] || this.keysPressed["shift"]) {
+      // None of our remaining shortcuts use combo keys so bail here
+      return;
+    }
+
+    if (this.keysPressed["w"] || this.keysPressed["a"]) {
+      return Turbo.visit("/words");
+    }
+    if (this.keysPressed["c"]) {
+      return Turbo.visit("/words?filter=cards_not_created&order=oldest_first");
+    }
+    if (this.keysPressed["n"]) {
+      const newWordBtton = document.querySelector("#new-word-form .new-word");
+      if (newWordBtton) {
+        return newWordBtton.click();
+      }
+      return Turbo.visit("/words/new");
+    }
+    if (this.keysPressed["f"]) {
+      return Turbo.visit("/words/search");
+    }
+    if (this.keysPressed["e"]) {
+      return Turbo.visit("/words/export");
+    }
+    if (this.keysPressed["i"]) {
+      return Turbo.visit("/words/import");
+    }
+    if (this.keysPressed["s"]) {
+      return document.querySelector("#stats-nav-link").click();
     }
   }
 }
