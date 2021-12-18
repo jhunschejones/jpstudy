d4b23b29f6b45a6d5dd4e0ad2661c17bd5300b70
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-12-17 19:23:32 -0800

    Add copy button and no new kanji message

--- app/javascript/controllers/kanji_controller.js
+++ app/javascript/controllers/kanji_controller.js
@@ -1,9 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "character" ]
+  static targets = [ "character", "checkIcon", "copyIcon" ]
 
   copy() {
     navigator.clipboard.writeText(this.characterTarget.innerText);
+    this.copyIconTarget.style.display = "none";
+    this.checkIconTarget.style.display = "block";
+    setTimeout(() => {
+      this.copyIconTarget.style.display = "block";
+      this.checkIconTarget.style.display = "none";
+    }, 2000);
   }
 }
