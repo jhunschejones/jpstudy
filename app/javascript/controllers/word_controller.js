9a2cc6deced66ed8d3daed9abb3f8053698e4a6a
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-01-31 15:45:10 -0600

    Sort by created at and id as well

--- app/javascript/controllers/word_controller.js
+++ app/javascript/controllers/word_controller.js
@@ -8,7 +8,9 @@ export default class extends Controller {
     applyJsFilters: Boolean,
     japaneseWord: String,
     englishWord: String,
-    addedOn: String,
+    addedAt: String,
+    createdAt: String,
+    databaseId: String,
     wordId: String
   }
 
@@ -69,23 +71,34 @@ export default class extends Controller {
   // return false if should not place, true after element is placed
   placeAmongExistingCards() {
     const allOtherWords = document.querySelectorAll(`.word:not(.skeleton-word):not(.${this.wordIdValue})`);
-    const wordsAddedOnDates = Array
+    const allOtherWordsSortableFields = Array
       .from(allOtherWords)
-      .map((word) => new Date(word.dataset.wordAddedOnValue));
+      .map((word) => {
+        return {
+          addedAt: new Date(word.dataset.wordAddedAtValue),
+          createdAt: new Date(word.dataset.wordCreatedAtValue),
+          databaseId: parseInt(word.dataset.wordDatabaseIdValue)
+        }
+      });
 
     // assuming we're in decending order here, as is the current usecase
-    const newestDate = wordsAddedOnDates[wordsAddedOnDates.length - 1];
-    const thisWordsDate = new Date(this.addedOnValue);
+    const newestSortableFields = allOtherWordsSortableFields[allOtherWordsSortableFields.length - 1];
+    const thisWordSortableFields = {
+      addedAt: new Date(this.addedAtValue),
+      createdAt: new Date(this.createdAtValue),
+      databaseId: parseInt(this.databaseIdValue)
+    };
+
     // If the newest date on the page is still earlier than this date, and there
     // is a multiple of the max words per page on the page, return false.
     // This will prevent us from adding new words when showing oldest first.
-    // the new word will then appear for real when the page is reached.
-    if (newestDate <= thisWordsDate && allOtherWords.length % 10 == 0) {
+    // The new word will then appear for real when the page is scrolled to.
+    if (newestSortableFields.addedAtValue <= thisWordSortableFields.addedAtValue && allOtherWords.length % 10 == 0) {
       return false;
     }
 
     // find where the date fits in the array of dates and place the card
-    const insertBeforeIndex = this.indexToInsertAt(wordsAddedOnDates, thisWordsDate);
+    const insertBeforeIndex = this.indexToInsertAt(allOtherWordsSortableFields, thisWordSortableFields);
     const thisTurboFrame = this.element.closest("turbo-frame");
     thisTurboFrame.parentNode.removeChild(thisTurboFrame);
     if (insertBeforeIndex == allOtherWords.length) {
@@ -93,7 +106,7 @@ export default class extends Controller {
       allOtherWords[allOtherWords.length - 1].closest("turbo-frame").parentNode.appendChild(thisTurboFrame);
     } else {
       // the word should go in the middle somewhere
-      allOtherWords[insertBeforeIndex].closest("turbo-frame").parentNode.appendChild(thisTurboFrame);
+      allOtherWords[insertBeforeIndex].closest("turbo-frame").parentNode.insertBefore(thisTurboFrame, allOtherWords[insertBeforeIndex].closest("turbo-frame"));
     }
     return true;
   }
@@ -102,7 +115,11 @@ export default class extends Controller {
   indexToInsertAt(array, newElement) {
     let newArray = array.concat(newElement);
     // Sort the new array from greatest to least.
-    newArray.sort((a, b) => b - a);
+    newArray.sort((a, b) => {
+      return (a.addedAt - b.addedAt || a.createdAt - b.createdAt || a.databaseId - b.databaseId);
+      // return (a.databaseId - b.databaseId || a.createdAt - b.createdAt || a.addedAt - b.addedAt);
+    });
+    // newArray.sort((a, b) => b - a);
     // Return the index of the new element which is now in the correct place in the new array.
     return newArray.indexOf(newElement);
   }
