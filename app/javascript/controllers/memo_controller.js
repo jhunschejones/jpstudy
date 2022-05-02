00fd7e9f2b43a02103afe10b5a67b07538492405
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-05-02 17:54:58 -0500

    Update memo save button disabled state when there are changes and to show auto-save

--- app/javascript/controllers/memo_controller.js
+++ app/javascript/controllers/memo_controller.js
@@ -1,31 +1,41 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "form", "content" ]
+  static targets = [ "saveButton", "form", "content" ]
 
   initialize() {
     this.previousContent = this.contentTarget.innerHTML;
     const trixEditor = this.formTarget.querySelector("trix-editor");
-    trixEditor.addEventListener("trix-change", this.autoSave.bind(this));
+    trixEditor.addEventListener("trix-change", this.contentChanged.bind(this));
+    // start the saved button in a disabled state to indicate no new changes
+    this.saveButtonTarget.disabled = true;
+  }
+
+  contentChanged() {
+    // set the saved button disabled state to indicate whether there were changes made
+    this.saveButtonTarget.disabled = !this.contentHasChanged();
+    this.autoSave();
   }
 
   autoSave() {
     // clear previous auto-save attempt when user enters more content
     clearTimeout(this.timeout);
 
-    const currentContent = this.contentTarget.innerHTML;
-    if (currentContent != this.previousContent) {
+    if (this.contentHasChanged()) {
       // start timeout for auto save
       this.timeout = setTimeout(() => {
         // execute auto-save after timeout
+        const contentBeingSaved = this.contentTarget.innerHTML;
         fetch(this.formTarget.action, {
           method: this.formTarget.method,
           body: new FormData(this.formTarget),
           headers: { Accept: "application/json" }
         }).then((response) => {
           if (response && response.ok) {
-            // update our pointer to the current state
-            this.previousContent = currentContent;
+            // update our pointer to the current state that was just saved
+            this.previousContent = contentBeingSaved;
+            // reset the saved button disabled state to indicate no new changes
+            this.saveButtonTarget.disabled = true;
             return;
           }
           // say something if the form was unable to be submitted
@@ -33,5 +43,9 @@ export default class extends Controller {
         })
       }, 5000);
     }
+  }
+
+  contentHasChanged() {
+    return this.contentTarget.innerHTML != this.previousContent;
   }
 }
